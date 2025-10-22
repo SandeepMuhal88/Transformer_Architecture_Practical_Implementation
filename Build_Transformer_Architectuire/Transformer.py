a2b028f5b6b3aa9e1b6e2f2fe72b1d8dfa7de9cf
Sandeepmuhal88 <sandeepmuhal9982@gmail.com>
2025-10-22 18:17:04 +0530

    Change the copde make input layer

---
+++ Build_Transformer_Architectuire/Transformer.py
@@ -0,0 +1,7 @@
+import torch
+import math
+import torch.nn as nn
+class Transformer:
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
+                 maximum_position_encoding, rate=0.1):
+        pass
